1aeabcc338aeea7f91839c974dabe31d3c278c2d
sanketsud21 <sanketsud21@gmail.com>
2020-03-09 00:05:51 +0530

    add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast=(latitude,longitude,callback)=>{
         }else if(body.error){
             callback('Unable to find the location',undefined)
         }else{
-            callback(undefined,'it is currently '+body.currently.temperature+' degree out.there is '+body.currently.precipProbability+' chance of the rain')
+            
+            callback(undefined,body.daily.data[0].summary+'it is currently '+body.currently.temperature+' degree out.The high today is '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow +'.There is '+body.currently.precipProbability+' chance of the rain')
         }
      })
 
